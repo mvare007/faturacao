5a74269234ec3a1a0207152d49b5535b1ceb1bac
marco varela <marco.varela12@gmail.com>
2021-08-11 14:58:09 +0100

    Added model translations

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Faturacao
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.i18n.default_locale = :pt
+    # rubocop:disable Rails/FilePath
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    # rubocop:enable Rails/FilePath
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
